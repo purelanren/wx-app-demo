c28b2623ede97fac0c5e40d28fb110731979d495
lushengteng <lushengteng@housechina.com>
2016-10-17 14:28:04 +0800

    Revert "Merge pull request #2 from purelanren/demo/jupai"
    This reverts commit d5632c2a0f31ccd47a191cf0555a8fb77a4e60e4, reversing
    changes made to 2a781544082f99bc96983e00b4a9f777d685fa32.

---
+++ README.md
@@ -0,0 +1,5 @@
+## 微信小程序
+
+
+### API
+请参阅官方文档：http://wxopen.notedown.cn/
--- utils/util.js
+++ utils/util.js
@@ -5,7 +5,7 @@ function formatTime(date) {
 
   var hour = date.getHours()
   var minute = date.getMinutes()
-  var second = date.getSeconds()
+  var second = date.getSeconds();
 
 
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
@@ -16,22 +16,6 @@ function formatNumber(n) {
   return n[1] ? n : '0' + n
 }
 
-function numberToChinese (num) {
-  var r, n, x = 0, zero = num % 10 === 0, v = num;
-  var unit = ['','','十','百','千','万'];
-  var n2c = ['零','一','二','三','四','五','六','七','八','九'];
-  if (v * 1 === 0) return n2c[0]
-  while (v > 0) {
-    x++;
-    n = v % 10;
-    v = window.Math.floor(v /= 10);
-    if (n === 0 && v % 10 === 0) continue;
-    r = (r ? n2c[n] + (n ? unit[x] : '') + r : n2c[n]);
-  }
-  return zero ? r.substr(0, r.length - 1) : r;
-}
-
 module.exports = {
-  formatTime: formatTime,
-  numberToChinese: numberToChinese
+  formatTime: formatTime
 }
